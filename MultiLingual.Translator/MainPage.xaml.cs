0b34713aa55e4d68a3f542e0cef59b0215433e7a
Tore Aurstad <tore.aurstad@gmail.com>
2023-09-20 13:25:28 +0200

    Init commit MAUI app with universal translator

---
+++ MultiLingual.Translator/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiLingual.Translator;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MultiLingual.Translator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MultiLingual.Translator;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
