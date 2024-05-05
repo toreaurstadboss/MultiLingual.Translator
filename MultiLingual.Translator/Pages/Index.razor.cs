129d3865334c8589f80c8600e8fc3bcaa6082102
Tore Aurstad <tore.aurstad@hemit.no>
2024-05-05 21:27:40 +0200

    Adding a refresh inside the transcript tab

--- MultiLingual.Translator/Pages/Index.razor.cs
+++ MultiLingual.Translator/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
 using MultiLingual.Translator.Lib;
 using MultiLingual.Translator.Lib.Models;
